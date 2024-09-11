c5392527aff20e7f329e6baa84e23783adac795c
Suryansh Singh <suryanshx25@gmail.com>
2024-09-11 14:53:37 +0530

    add unit test job

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ def sub(a, b):
     return a-b
 
 
-result1 = add(4, 5)
-result2 = sub(4, 1)
+def mul(a, b):
+    return a * b
 
-print(result1, result2)
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
+
+    print(result1, result2)
+
