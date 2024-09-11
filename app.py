c481d8c6746a3bf8dafecced7899f36e88c5a2b6
Suryansh Singh <suryanshx25@gmail.com>
2024-09-11 14:56:36 +0530

    fixed minor leting issues

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ def sub(a, b):
 
 
 def mul(a, b):
-    return a * b
+    return a*b
+
 
 if __name__ == "__main__":
     result1 = add(4, 5)
     result2 = sub(4, 1)
 
     print(result1, result2)
-
